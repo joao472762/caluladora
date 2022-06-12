c52c5d5df1475ae826cf3704acbcb877126990f1
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-06-12 09:54:09 -0300

    update
    add a new function that allow that user type directly on the screen

--- javaScripts/script.js
+++ javaScripts/script.js
@@ -1,23 +1,90 @@
+let result = document.getElementById('result')
+
+
+let HandleKeyboard = {
+    updateDisplay(){
+        if(HandleKeyboard.isMathChar()){
+            return; 
+        }
+        
+        let resultArray = result.value.split('')
+
+        resultArray.pop()
+        
+        resultArray = Format.formatArray(resultArray)
+    
+        result.value = resultArray
+    },
+    
+    isMathChar(){
+        let resultArray = result.value.split('')
+        let lastChar = (resultArray[resultArray.length -1])
+        let number = parseInt(lastChar)
+        if(
+           HandleKeyboard.checkMathOperator(lastChar)
+     
+        ){
+            return true
+        }
+
+        let stringNumber = number.toString()
+
+
+        if(stringNumber == 'NaN'){
+            return false
+        }
+        return true
+   
+    },
+    checkMathOperator(operator = ''){
+        if(
+            operator == '*' ||
+            operator == '/' ||
+            operator == '-' ||
+            operator == '+'
+     
+        ){
+            return true
+        }
+        return false
+    }
+
+}
+
+let Format = {
+     formatArray(array = []){
+        let arrayFormated = array.join(separator='')
+        arrayFormated = Format.formatString(arrayFormated)
+        return arrayFormated
+    },
+    
+    formatString(string = ''){
+        let stringFormated = string.replace(/ /g, '')
+        return stringFormated
+    } 
+}
+
 let Dom ={
     handleAddChar(char){
-        result.textContent += char
+        result.value += char
     },
 
     deleteAllChars(){
-        result.textContent = ''
+        result.value = ''
     },
 
     deleteLastChar(){
-        let content = result.textContent
-        result.textContent = content.substring(0, content.length -1)
+        let content = result.value
+        result.value = content.substring(0, content.length -1)
     },
 
     handleTotal(){ 
-        if(result.textContent.length === 0){
+        if(result.value.length === 0){
             return
         }
-        result.innerHTML = eval(result.textContent)
+        result.value = eval(result.value)
     }
     
 }
 
+result.addEventListener('keyup',HandleKeyboard.updateDisplay)
